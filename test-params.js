4387733130667abfee8fcf6eb07d4d03eb1642d4
deenyy <deenyy@yandex.ru>
2026-02-25 17:16:04 +0300

    chore: cleanup root directory and organize scripts/tests

--- test-params.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const doc = JSON.parse(fs.readFileSync('Справочные_материалы/swagger', 'utf8'));
-console.log(JSON.stringify(doc.paths['/job/v2/vacancies'].get.parameters, null, 2));
